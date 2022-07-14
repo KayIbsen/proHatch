db790a55bc712f2360b1f97221d384f82111945b
Søren Ibsen <kay.ibsen@gmail.com>
2022-07-14 10:15:52 +0200

    initial default function added. Being confugured

---
+++ SensorService/SensorRecord_DTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorService
+{
+    public class SensorRecord_DTO
+    {
+
+        public Guid Id { get; set; }
+        public Guid UnitId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public int PPM { get; set; }
+
+    }
+}
