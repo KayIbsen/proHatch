c853b5e1568b32e296ce1d6093578ede331ffdb7
Søren Ibsen <kay.ibsen@gmail.com>
2022-07-26 09:05:14 +0200

    MVC WebApp initialized

---
+++ proHatchWebApp/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proHatchWebApp.Models;
+
+namespace proHatchWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public LoginController()
+        {
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Login_DTO login_DTO)
+        {
+            return View();
+        }
+
+    }
+}
